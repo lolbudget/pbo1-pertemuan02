5d5e5491fcde56e4f0d88227a04d4524a36d4503
lolbudget <elevenqim@gmail.com>
2020-04-21 13:17:45 +0800

    Update InputOutput.java

--- InputOutput.java
+++ InputOutput.java
@@ -2,6 +2,8 @@ public class InputOutput{
  public static void main(String [] args){
 
   Scanner scanner = new Scanner(System.in);
-
+  String nama;
+  System.out.print("Masukan nama anda : ");
+  nama = scanner.nextLine();
  }
 }
